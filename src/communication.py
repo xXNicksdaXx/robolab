2a5521103813278c8b5d90d12c83987e32a22c95
Meng Zhang <mengz.Simon@gmail.com>
2021-08-29 22:51:03 +0200

    MQTT-Prameter konfigurieren

--- src/communication.py
+++ src/communication.py
@@ -6,49 +6,9 @@ import platform
 import ssl
 import paho.mqtt.client as mqtt
 
-# this is a helper method that catches errors and prints them
-# it is necessary because on_message is called by paho-mqtt in a different thread and exceptions
-# are not handled in that thread
-#
-# you don't need to change this method at all
-def on_message_excepthandler(client, data, message):
-    try:
-        on_message(client, data, message)
-    except:
-        import traceback
-        traceback.print_exc()
-        raise
-
-# Callback function for receiving messages
-def on_message(client, data, message):
-    print('Got message with topic "{}":'.format(message.topic))
-    data = json.loads(message.payload.decode('utf-8'))
-    print(json.dumps(data, indent=2))
-    print("\n")
-
-
-# Basic configuration of MQTT
-client = mqtt.Client(client_id="explprer/125", clean_session=False, protocol=mqtt.MQTTv31)
-
-client.on_message = on_message_excepthandler # Assign pre-defined callback function to MQTT client
-client.tls_set(tls_version=ssl.PROTOCOL_TLS)
-client.username_pw_set('explorer/125', password='IWDwkt9Ao3') # Your group credentials
-client.connect('mothership.inf.tu-dresden.de', port=8883)
-client.subscribe('explorer/125', qos=1) # Subscribe to topic explorer/xxx
-
-# Start listening to incoming messages
-client.loop_start()
-
-while True:
-	input('Press Enter to continue...\n')
-
-client.loop_stop()
-client.disconnect()
-
 # Fix: SSL certificate problem on macOS
 if all(platform.mac_ver()):
     from OpenSSL import SSL
-
 
 class Communication:
     """
@@ -56,6 +16,20 @@ class Communication:
     Feel free to add functions and update the constructor to satisfy your requirements and
     thereby solve the task according to the specifications
     """
+
+    # this is a helper method that catches errors and prints them
+    # it is necessary because on_message is called by paho-mqtt in a different thread and exceptions
+    # are not handled in that thread
+    #
+    # you don't need to change this method at all
+    def on_message_excepthandler(client, data, message):
+        try:
+            on_message(client, data, message)
+        except:
+            import traceback
+            traceback.print_exc()
+            raise
+
 
     def __init__(self, mqtt_client, logger):
         """
@@ -68,8 +42,22 @@ class Communication:
         self.client.tls_set(tls_version=ssl.PROTOCOL_TLS)
         self.client.on_message = self.safe_on_message_handler
         # Add your client setup here
+        self.client = mqtt.Client(client_id="MQTT_FX_Client", clean_session=False, protocol=mqtt.MQTTv31)
+
+        self.client.username_pw_set('125', password='IWDwkt9Ao3')  # Your group credentials
+        self.client.connect('mothership.inf.tu-dresden.de', port=8883)
+        self.client.subscribe('explorer/125', qos=1)  # Subscribe to topic explorer/xxx
 
         self.logger = logger
+
+    # Start listening to incoming messages
+    client.loop_start()
+
+    while True:
+        input('Press Enter to continue...\n')
+
+    client.loop_stop()
+    client.disconnect()
 
     # DO NOT EDIT THE METHOD SIGNATURE
     def on_message(self, client, data, message):
@@ -80,7 +68,7 @@ class Communication:
         :param message: Object
         :return: void
         """
-        payload = json.loads(message.payload.decode('utf-8'))
+        payload = json.loads(message.payload.decode('utf-8'))  # str -> dict
         self.logger.debug(json.dumps(payload, indent=2))
 
         # YOUR CODE FOLLOWS (remove pass, please!)
