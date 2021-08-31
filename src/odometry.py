1332d836c67e5a9e6084bfbd1ad626d44ac82728
linar <linarazouk@gmail.com>
2021-08-31 13:01:59 +0200

    fix odometry

--- src/odometry.py
+++ src/odometry.py
@@ -4,17 +4,24 @@ import math
 
 
 class Odometry:
+
+
+    alpha = 0
+    beta = 0
+    s = 0
+
+
     def __init__(self):
         self.a = 12
-        # self.distance_per_tick = math.pi * 5.6
-        self.distance_per_tick = 0.015
+        self.distance_per_tick = (math.pi * 5.6)/360
+        self.distance = None
+        #self.distance_per_tick = 0.015
+
 
     def calculate(self, positions, old_X, old_Y, old_Dir):
 
         print("----calculate coordinates----")
         print("old Coordinates: " + str(old_X) + ", " + str(old_Y))
-
-        old_Dir = math.radians(old_Dir)
 
         for p in positions:
 
@@ -25,30 +32,30 @@ class Odometry:
             r_distance = p[1] * self.distance_per_tick
 
             # calculate alpha & beta
-            alpha = (l_distance - r_distance) / self.a
+            alpha = (r_distance - l_distance) / self.a
             print("alpha :......" + str(alpha) + "........")
             beta = alpha / 2
 
             if alpha == 0:
-                s = r_distance
+                self.distance = r_distance
             else:
-                s = (r_distance + l_distance)/alpha * math.sin(beta)
+                self.distance = ((r_distance + l_distance)/alpha) * math.sin(beta)
 
             # maybe radian()
-            delta_X = -math.sin(math.radians(int(old_Dir)) + beta) * s
-            delta_Y = math.cos(math.radians(int(old_Dir)) + beta) * s
+            delta_X = math.sin(math.radians(old_Dir) + beta) * self.distance
+            delta_Y = math.cos(math.radians(old_Dir) + beta) * self.distance
 
             print("deltaX, deltaY:....." + str(delta_X) + "," + str(delta_Y) + "..........")
 
             # maybe round()
-            new_Dir = math.radians(int(old_Dir)) + alpha
+            new_Dir = math.radians(old_Dir) - alpha
             new_X = old_X + delta_X
             new_Y = old_Y + delta_Y
 
             old_X = new_X
             old_Y = new_Y
-            old_Dir = new_Dir
+            old_Dir = math.degrees(new_Dir)
 
         print("new Coordinates: " + str(old_X) + ", " + str(old_Y))
-        return ((old_X, old_Y), math.degrees(old_Dir))
+        return ((old_X, old_Y), old_Dir)
 
